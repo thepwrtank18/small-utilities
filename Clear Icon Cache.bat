b0c4913089a14113f6a9b78b0b6848abb77e7804
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2020-12-16 17:54:33 -0500

    Add files via upload

--- Clear Icon Cache.bat
+++ Clear Icon Cache.bat
@@ -1,24 +1,33 @@
 @echo off
-goto check_Permissions
+goto CheckPerms
 
-:check_Permissions
+:CheckPerms
     echo Administrative permissions required. Detecting permissions...
 
     net session >nul 2>&1
     if %errorLevel% == 0 (
         echo Administrative permissions confirmed. Press any key to clear icon cache.
         pause >nul
-        echo Clearing icon cache...
-        cd %localappdata%
-        ie4uinit.exe -ClearIconCache
-        taskkill /im explorer.exe /f
-        cd %SystemRoot%
-        timeout /t 5 /nobreak
-        start explorer.exe
-        echo Successfully cleared icon cache. Press any key to exit.
+        goto ClearCache
+        goto RestartEplorer
         
     ) else (
         echo Permission error. Please run with administrator priveleges. Press any key to exit.
+        pause >nul
+        exit
     )
 
+
+:ClearCache
+    echo Clearing icon cache...
+    cd %localappdata%
+    ie4uinit.exe -ClearIconCache
+
+:RestartExplorer
+    taskkill /im explorer.exe /f
+    cd %SystemRoot%
+    timeout /t 5 /nobreak
+    start explorer.exe
+    echo Successfully cleared icon cache. Press any key to exit.
     pause >nul
+    exit
