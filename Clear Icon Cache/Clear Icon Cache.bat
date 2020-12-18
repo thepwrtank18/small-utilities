a2c5e5a02327edaeeaa976c33031d27b9d199200
TheTank20 <57580668+thepwrtank18@users.noreply.github.com>
2020-12-17 19:29:03 -0500

    Add files via upload

--- Clear Icon Cache/Clear Icon Cache.bat
+++ Clear Icon Cache/Clear Icon Cache.bat
@@ -26,11 +26,37 @@ goto CheckPerms
     cd %SystemRoot%
     timeout /t 5 /nobreak
     start explorer.exe
-    echo Successfully cleared icon cache. Press any key to exit.
-    pause >nul
-    exit
+    echo Successfully cleared icon cache.
+    goto ClearCacheChoice
 
 :PermsError
     echo Permission error. Please run with administrator priveleges. Press any key to exit.
+    pause >nul
+    exit
+
+:ClearCacheChoice
+    set /p menu="If this doesn't work, would you like to try another method? [Y/N]"
+        if %menu%==Y goto ClearCacheAlt
+        if %menu%==y goto ClearCacheAlt
+        if %menu%==N goto ClearCacheNo
+        if %menu%==n goto ClearCacheNo
+        cls
+
+:ClearCacheAlt
+    echo Using alternate method...
+    cd %localappdata%
+    del /f "IconCache.db"
+    goto RestartExplorerAlt
+    exit
+
+:ClearCacheNo
+    exit
+
+:RestartExplorerAlt
+    taskkill /im explorer.exe /f
+    cd %SystemRoot%
+    timeout /t 5 /nobreak
+    start explorer.exe
+    echo Successfully cleared icon cache. Press any key to exit.
     pause >nul
     exit
